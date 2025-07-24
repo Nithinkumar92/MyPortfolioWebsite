a8cc58054834e01cdc50c42aeb3d801b18faad60
nithinkumar92 <nithinkumar57992@gmail.com>
2025-07-24 16:51:10 +0530

    message

--- server/index.js
+++ server/index.js
@@ -43,7 +43,7 @@ app.post('/api/contact', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5001; // Changed to 5001
+const PORT = process.env.PORT || 1000 ||5001; // Changed to 5001
 const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI)
